81ffe4c7e96c65d8e10a579ac06f2cdc467999da
Jonathan Wu <jwu20@stuy.edu>
2021-01-04 12:10:13 -0500

    added MakeStars.java skeleton code

---
+++ MakeStars.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class MakeStars {
+  public static void main(String[] args) {
+    Scanner myScanner = new Scanner(System.in);
+    while (myScanner.hasNextLine()) {
+      System.out.println(myScanner.nextLine());
+    }
+  }
+}
