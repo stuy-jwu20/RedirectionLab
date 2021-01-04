c475873fb75d1c0cea23ed75dba8a7c77045213d
Jonathan Wu <jwu20@stuy.edu>
2021-01-04 13:49:24 -0500

    added three skeleton methods for piglatin

--- PigLatin.java
+++ PigLatin.java
@@ -3,6 +3,18 @@ import java.util.*;
 
 public class PigLatin {
   public static void main(String[] args) {
+
+  }
+
+  public static String pigLatinSimple(string s) {
+
+  }
+
+  public static String pigLatin(String s) {
+
+  }
+
+  public static String pigLatinBest(String s) {
     
   }
 }
