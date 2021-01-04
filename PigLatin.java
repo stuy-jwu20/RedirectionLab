9b46f74df47d038be530db498af546f15439ecd5
Jonathan Wu <jwu20@stuy.edu>
2021-01-04 14:33:07 -0500

    checked if string is at least one letter in piglatinsimple

--- PigLatin.java
+++ PigLatin.java
@@ -3,22 +3,28 @@ import java.util.*;
 
 public class PigLatin {
   public static void main(String[] args) {
-
+    System.out.println(pigLatinSimple("mock"));
+    System.out.println(pigLatinSimple("aaron"));
   }
 
   public static String pigLatinSimple(string s) {
     String vowels = "aeiou";
-    if (vowels.indexOf(s.charAt(0)) != -1) {
-      String newWord = s + "hay";
-      newWord = newWord.toLowerCase();
-      return newWord;
-    } else {
-      String newWord = s.substring(1,s.length()) + s.substring(0,1) + "ay";
-      return newWord;
+    if (s.length() >= 1) {
+      if (vowels.indexOf(s.charAt(0)) != -1) {
+        String newWord = s + "hay";
+        newWord = newWord.toLowerCase();
+        return newWord;
+      } else {
+        String newWord = s.substring(1,s.length()) + s.substring(0,1) + "ay";
+        return newWord;
+      }
     }
   }
 
   public static String pigLatin(String s) {
+    if (s.length() >= 1) {
+      String[] digraph
+    }
 
   }
 
