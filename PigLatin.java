87b395d0378737cd89ca8cbe72621a4ab027c642
Jonathan Wu <jwu20@stuy.edu>
2021-01-04 15:52:53 -0500

    converted string to lowercase first to make conversion consistent

--- PigLatin.java
+++ PigLatin.java
@@ -24,18 +24,19 @@ public class PigLatin {
     //
     System.out.println(pigLatinBest("4chan"));
     System.out.println(pigLatinBest("fish!"));
-    System.out.println(pigLatinBest("Science"));
+    System.out.println(pigLatinBest("science"));
   }
 
   public static String pigLatinSimple(String s) {
+    String lowerS = s.toLowerCase();
     String vowels = "aeiou";
-    if (s.length() >= 1) {
-      if (vowels.indexOf(s.charAt(0)) != -1) {
-        String newWord = s + "hay";
+    if (lowerS.length() >= 1) {
+      if (vowels.indexOf(lowerS.charAt(0)) != -1) {
+        String newWord = lowerS + "hay";
         newWord = newWord.toLowerCase();
         return newWord;
       } else {
-        String newWord = s.substring(1,s.length()) + s.substring(0,1) + "ay";
+        String newWord = lowerS.substring(1,lowerS.length()) + lowerS.substring(0,1) + "ay";
         newWord = newWord.toLowerCase();
         return newWord;
       }
@@ -44,41 +45,43 @@ public class PigLatin {
   }
 
   public static String pigLatin(String s) {
-    if (s.length() >= 1) {
+    String lowerS = s.toLowerCase();
+    if (lowerS.length() >= 1) {
       String[] digraph = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
       String vowels = "aeiou";
-      if (Arrays.asList(digraph).indexOf(s.substring(0,2)) != -1) {
-        String newWord = s.substring(2,s.length()) + s.substring(0,2) + "ay";
+      if (Arrays.asList(digraph).indexOf(lowerS.substring(0,2)) != -1) {
+        String newWord = lowerS.substring(2,lowerS.length()) + lowerS.substring(0,2) + "ay";
         newWord.toLowerCase();
         return newWord;
       } else {
-        return pigLatinSimple(s);
+        return pigLatinSimple(lowerS);
       }
     }
     return s;
   }
 
   public static String pigLatinBest(String s) {
-    if (s.length() >= 1) {
+    String lowerS = s.toLowerCase();
+    if (lowerS.length() >= 1) {
       String theNumbers = "123456789";
       String nonSymbols = "abcdefghijklmnopqrstuvwxyz123456789";
-      if (theNumbers.indexOf(s.substring(0,1)) != -1) {
-        return s.toLowerCase();
+      if (theNumbers.indexOf(lowerS.substring(0,1)) != -1) {
+        return lowerS.toLowerCase();
       }
-      else if (nonSymbols.indexOf(s.substring(s.length()-1, s.length())) == -1) {
+      else if (nonSymbols.indexOf(lowerS.substring(lowerS.length()-1, lowerS.length())) == -1) {
         String[] digraph = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
         String vowels = "aeiou";
-        if (Arrays.asList(digraph).indexOf(s.substring(0,2)) != -1) {
-          String newWord = s.substring(2,s.length()-1) + s.substring(0,2) + "ay" + s.substring(s.length()-1, s.length());
+        if (Arrays.asList(digraph).indexOf(lowerS.substring(0,2)) != -1) {
+          String newWord = lowerS.substring(2,lowerS.length()-1) + lowerS.substring(0,2) + "ay" + lowerS.substring(lowerS.length()-1, lowerS.length());
           newWord = newWord.toLowerCase();
           return newWord;
         }
-        else if (vowels.indexOf(s.charAt(0)) != -1) {
-          String newWord = s.substring(0,s.length()-1) + "hay" + s.substring(s.length()-1, s.length());
+        else if (vowels.indexOf(lowerS.charAt(0)) != -1) {
+          String newWord = lowerS.substring(0,lowerS.length()-1) + "hay" + lowerS.substring(lowerS.length()-1, lowerS.length());
           newWord = newWord.toLowerCase();
           return newWord;
         } else {
-          String newWord = s.substring(1,s.length()-1) + s.substring(0,1) + "ay" + s.substring(s.length()-1, s.length());
+          String newWord = lowerS.substring(1,lowerS.length()-1) + lowerS.substring(0,1) + "ay" + lowerS.substring(lowerS.length()-1, lowerS.length());
           newWord = newWord.toLowerCase();
           return newWord;
         }
