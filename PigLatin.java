e5518d58d02353b26e51cafba65e16429aa80fee
Jonathan Wu <jwu20@stuy.edu>
2021-01-04 14:37:52 -0500

    added pigLatin methods

--- PigLatin.java
+++ PigLatin.java
@@ -23,9 +23,21 @@ public class PigLatin {
 
   public static String pigLatin(String s) {
     if (s.length() >= 1) {
-      String[] digraph
+      String[] digraph = ["bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"];
+      if (digraph.indexOf(s.substring(0,2)) != -1) {
+        String newWord = s.substring(2,s.length()) + s.substring(0,2) + "ay";
+        return newWord;
+      }
+      else if (vowels.indexOf(s.charAt(0)) != -1) {
+        String vowels = "aeiou";
+        String newWord = s + "hay";
+        newWord = newWord.toLowerCase();
+        return newWord;
+      } else {
+        String newWord = s.substring(1,s.length()) + s.substring(0,1) + "ay";
+        return newWord;
+      }
     }
-
   }
 
   public static String pigLatinBest(String s) {
