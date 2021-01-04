0ccda63fdc6dd3a4f3e61d178ac391524576eebc
Jonathan Wu <jwu20@stuy.edu>
2021-01-04 14:26:04 -0500

    added pigLatinSimple method

--- PigLatin.java
+++ PigLatin.java
@@ -7,7 +7,15 @@ public class PigLatin {
   }
 
   public static String pigLatinSimple(string s) {
-
+    String vowels = "aeiou";
+    if (vowels.indexOf(s.charAt(0)) != -1) {
+      String newWord = s + "hay";
+      newWord = newWord.toLowerCase();
+      return newWord;
+    } else {
+      String newWord = s.substring(1,s.length()) + s.substring(0,1) + "ay";
+      return newWord;
+    }
   }
 
   public static String pigLatin(String s) {
@@ -15,6 +23,6 @@ public class PigLatin {
   }
 
   public static String pigLatinBest(String s) {
-    
+
   }
 }
