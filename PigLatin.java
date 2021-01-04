7f1891b36fb12c4b7c5816e3ded88dca0cc56a7e
Jonathan Wu <jwu20@stuy.edu>
2021-01-04 15:42:00 -0500

    optimized code greatly, cut some redundant code

--- PigLatin.java
+++ PigLatin.java
@@ -3,16 +3,28 @@ import java.util.*;
 
 public class PigLatin {
   public static void main(String[] args) {
+    // Scanner myScanner = new Scanner (System.in);
+    // while (myScanner.hasNextLine()) {
+    //   String lineData = (myScanner.nextLine());
+    //   Scanner newScanner = new Scanner (lineData);
+    //   while (newScanner.hasNext()){
+    //     String wordData = newScanner.next();
+    //     System.out.print(pigLatinBest(wordData));
+    //     System.out.print(" ");
+    //   }
+    //   System.out.print("\n");
+    // }
+    //
     System.out.println(pigLatinSimple("mock"));
     System.out.println(pigLatinSimple("aaron"));
     //
     System.out.println(pigLatin("the"));
     System.out.println(pigLatin("emu"));
-    System.out.println(pigLatin("hats"));
+    System.out.println(pigLatin("science"));
     //
     System.out.println(pigLatinBest("4chan"));
     System.out.println(pigLatinBest("fish!"));
-    System.out.println(pigLatinBest("fish"));
+    System.out.println(pigLatinBest("Science"));
   }
 
   public static String pigLatinSimple(String s) {
@@ -39,15 +51,8 @@ public class PigLatin {
         String newWord = s.substring(2,s.length()) + s.substring(0,2) + "ay";
         newWord.toLowerCase();
         return newWord;
-      }
-      else if (vowels.indexOf(s.charAt(0)) != -1) {
-        String newWord = s + "hay";
-        newWord = newWord.toLowerCase();
-        return newWord;
       } else {
-        String newWord = s.substring(1,s.length()) + s.substring(0,1) + "ay";
-        newWord = newWord.toLowerCase();
-        return newWord;
+        return pigLatinSimple(s);
       }
     }
     return s;
@@ -61,8 +66,7 @@ public class PigLatin {
         return s.toLowerCase();
       }
       else if (nonSymbols.indexOf(s.substring(s.length()-1, s.length())) == -1) {
-        String[] digraph = {"bl",
-        "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+        String[] digraph = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
         String vowels = "aeiou";
         if (Arrays.asList(digraph).indexOf(s.substring(0,2)) != -1) {
           String newWord = s.substring(2,s.length()-1) + s.substring(0,2) + "ay" + s.substring(s.length()-1, s.length());
@@ -79,22 +83,7 @@ public class PigLatin {
           return newWord;
         }
       } else {
-          String[] digraph = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
-          String vowels = "aeiou";
-          if (Arrays.asList(digraph).indexOf(s.substring(0,2)) != -1) {
-            String newWord = s.substring(2,s.length()) + s.substring(0,2) + "ay";
-            newWord.toLowerCase();
-            return newWord;
-          }
-          else if (vowels.indexOf(s.charAt(0)) != -1) {
-            String newWord = s + "hay";
-            newWord = newWord.toLowerCase();
-            return newWord;
-          } else {
-            String newWord = s.substring(1,s.length()) + s.substring(0,1) + "ay";
-            newWord = newWord.toLowerCase();
-            return newWord;
-          }
+        return pigLatin(s);
       }
     }
     return s;
